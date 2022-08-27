545670041c274a5d318f5ee2c82be87807d61e18
Hamza Susam <hamza.susam@jetlink.io>
2022-08-27 20:33:49 +0300

    search button && on key press

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './searchbar.css'
 
-const SearchBar = () => {
-    return (
-        <div className="search-bar-container ui input">
-            <input type="text" placeholder="Search..."/>
-        </div>
-    );
+class SearchBar extends Component {
+
+    state ={
+        search:""
+    };
+    inputOnchanged(event){
+        console.log(event.target.value);
+        this.setState({
+            search:event.target.value
+        });
+    }
+
+    searchImage = () => {
+        console.log("başarılı");
+    }
+
+    render() {
+        return (
+            <div className="search-bar-container ui input">
+                <input type="text" placeholder="Search..." onKeyPress={(e) => {
+                    if(e.key == 'Enter'){
+                        this.searchImage();
+                    }
+                }} onChange={this.inputOnchanged.bind(this)}/>
+                <button className="ui icon button" onClick={this.searchImage}>
+                    <i className="search icon"></i>
+                </button>
+            </div>
+        )
+    }
 };
 
 export default SearchBar;
