7388cee8f0ee17cc676a97b05e928f52f64c0d11
Hamza Susam <hamza.susam@jetlink.io>
2022-08-27 22:19:04 +0300

    image list

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar'
 import './app.css'
 import axios from 'axios';
+import ImageList from './Image/ImageList';
 
 class App extends Component {
+  state = {
+    images:[]
+  }
 
-  onSearchImage = search => {
+  onSearchImage = async(search) => {
     console.log("App: " + search);
-    axios.get('https://api.unsplash.com/search/photos',{
+    const result = await axios.get('https://api.unsplash.com/search/photos',{
       params:{
         query : search
       },
       headers:{
         Authorization : "Client-ID xcDXwAswX_NPuZq9PJQXCKx1OktVyhlVXxd9wm_QQOk"
       }
-    }).then((result) => {
-      console.log(result);
+    });
+    // console.log(result.data.results);
+    this.setState({
+      images:result.data.results
     });
   }
 
@@ -23,6 +29,7 @@ class App extends Component {
     return (
       <div className='app-container'>
         <SearchBar onSearchImage={this.onSearchImage}></SearchBar>
+        <ImageList/>
       </div>
     );
   }
---
+++ src/components/Image/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageList(props) {
+    return (
+        <div>
+            imageList
+        </div>
+    );
+}
+
+export default ImageList;
